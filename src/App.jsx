a74abe67c2e1b8514da1c5fecb028e6b3fe9ffd3
Liam-M-Dev <liammassey21@gmail.com>
2023-10-04 18:23:43 +1100

    Boiler plate routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import LoginPage from './pages/LoginPage'
+import SignUpPage from './pages/SignUpPage'
 
 function App() {
   
 
   return (
-    <div><h1>hello world</h1></div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LoginPage/>} />
+        <Route path='/signup' element={<SignUpPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/SignUpPage.jsx
@@ -0,0 +1,6 @@
+
+export default function SignUpPage(){
+    return(
+        <div><h1>Sign Up</h1></div>
+    )
+}
