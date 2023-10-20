23bb59ac6ee933a9eaa04a98a08be650cd033e68
Liam-M-Dev <liammassey21@gmail.com>
2023-10-20 16:14:29 +1100

    Add basic form structure

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -5,18 +5,30 @@ export default function LoginForm(){
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    // const handleChange = async (event) => {
-    //     event.preventDefault();
-
-        
-    // }
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        // Add functionality to send user details to back-end server
+        // confirm user is logged in correctly to redirect to welcome page
+    }
 
     return(
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>Email: </label>
-            <input type="email" name="email" placeholder="email" value={email} onChange={(e) => {setEmail(e.target.value)}}/>
+            <input type="email" 
+                name="email" 
+                placeholder="email" 
+                value={email} 
+                onChange={(e) => {setEmail(e.target.value)}}
+                required={true}
+            />
             <label>Password: </label>
-            <input type="password" name="password" placeholder="password" value={password} onChange={(e) => {setPassword(e.target.value)}}/>
+            <input type="password" 
+                name="password" 
+                placeholder="password" 
+                value={password} 
+                onChange={(e) => {setPassword(e.target.value)}}
+                required={true}
+            />
             <button>Login</button>
         </form>
     )
